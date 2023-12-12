a043e260ccfe31b96b5cb75eeba7efcfc164d4bd
Yakitori <89288375+Oni-giri@users.noreply.github.com>
2023-12-12 02:54:18 +0200

    add mock contracts for testing

---
+++ tests/mocks/SafeMock.sol
@@ -0,0 +1,43 @@
+// SDPX License Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../interfaces/IPool.sol";
+
+
+contract SafeMock {
+    function executeCall(
+        address target,
+        bytes calldata data
+    ) external returns (bool success, bytes memory result) {
+        (success, result) = target.call(data);
+    }
+
+    function executeCallWithValue(
+        address target,
+        bytes calldata data
+    ) external payable returns (bool success, bytes memory result) {
+        (success, result) = target.call(data);
+    }
+
+    function approveToken(address token, address to, uint256 amount) external {
+        IERC20(token).approve(to, amount);
+    }
+
+    function supplyToPool(
+        address pool,
+        address token,
+        uint256 amount
+    ) external {
+        IERC20(token).transferFrom(msg.sender, pool, amount);
+        IPool(pool).deposit(token, amount, msg.sender, 0);
+    }
+
+    function borrowFromPool(
+        address pool,
+        address token,
+        uint256 amount
+    ) external {
+        IPool(pool).borrow(token, amount, 2, 0, msg.sender);
+    }
+}
